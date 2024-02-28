b30980ab73306992a09c4fecb12a5a034b37ac5c
Sophiaa1204 <sihanwangsophia@gmail.com>
2024-02-28 15:44:38 -0500

    feat: logger

---
+++ blog_java_backend/src/test/java/com/sophia/blog_java_backend/LoggerTests.java
@@ -0,0 +1,33 @@
+package com.sophia.blog_java_backend;
+
+import com.sophia.blog_java_backend.dao.AlphaDao;
+import com.sophia.blog_java_backend.service.AlphaService;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SpringBootTest
+@ContextConfiguration(classes = BlogJavaBackendApplication.class)
+public class LoggerTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTests.class);
+
+    @Test
+    public void testLogger() {
+        System.out.println(logger.getName());
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+}

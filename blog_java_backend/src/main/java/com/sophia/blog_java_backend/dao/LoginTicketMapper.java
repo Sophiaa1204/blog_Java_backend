09a78463f95ddcc87a82907b158b03ee48d9a93d
Sophiaa1204 <sihanwangsophia@gmail.com>
2024-03-02 21:15:31 -0500

    feat:login, logout

---
+++ blog_java_backend/src/main/java/com/sophia/blog_java_backend/dao/LoginTicketMapper.java
@@ -0,0 +1,32 @@
+package com.sophia.blog_java_backend.dao;
+
+import com.sophia.blog_java_backend.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Service;
+
+@Mapper
+public interface LoginTicketMapper {
+    @Insert({
+            "insert into login_ticket (user_id, ticket, status, expired) ",
+            "values(#{userId}, #{ticket}, #{status}, #{expired})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertLoginTicket(LoginTicket loginTicket);
+
+    //依据Ticket来查（知道是哪个用户在登录，在访问）
+    @Select({
+            "select id, user_id, ticket, status, expired ",
+            "from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+    @Update({
+            "<script>",
+            "update login_ticket set status=#{status} where ticket = #{ticket} ",
+            "<if test=\"ticket!=null\">",
+            "and 1=1 ",
+            "</if>",
+            "</script>"
+    })
+    int updateStatus(String ticket, int status);
+
+}
--- blog_java_backend/src/test/java/com/sophia/blog_java_backend/MapperTests.java
+++ blog_java_backend/src/test/java/com/sophia/blog_java_backend/MapperTests.java
@@ -1,8 +1,10 @@
 package com.sophia.blog_java_backend;
 
 import com.sophia.blog_java_backend.dao.DiscussPostMapper;
+import com.sophia.blog_java_backend.dao.LoginTicketMapper;
 import com.sophia.blog_java_backend.dao.UserMapper;
 import com.sophia.blog_java_backend.entity.DiscussPost;
+import com.sophia.blog_java_backend.entity.LoginTicket;
 import com.sophia.blog_java_backend.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectUser(){
@@ -72,5 +77,26 @@ public class MapperTests {
 
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket() {
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc",1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
     }
 }

a4cb912ef1bc0b9ebda6645205fc4a5f0514e45b
Sophiaa1204 <sihanwangsophia@gmail.com>
2024-03-05 19:01:37 -0500

    feat: comment detail

---
+++ blog_java_backend/src/main/java/com/sophia/blog_java_backend/dao/CommentMapper.java
@@ -0,0 +1,14 @@
+package com.sophia.blog_java_backend.dao;
+
+import com.sophia.blog_java_backend.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+    int selectCountByEntity(int entityType, int entityId);
+
+}
---
+++ blog_java_backend/src/main/java/com/sophia/blog_java_backend/service/CommentService.java
@@ -0,0 +1,22 @@
+package com.sophia.blog_java_backend.service;
+
+import com.sophia.blog_java_backend.dao.CommentMapper;
+import com.sophia.blog_java_backend.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentMapper commentMapper;
+
+    public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
+        return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
+    }
+
+    public int findCommentCount(int entityType, int entityId) {
+        return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+}

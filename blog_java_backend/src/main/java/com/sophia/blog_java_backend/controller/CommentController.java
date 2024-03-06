afb62cd8557d5808b2fd5379d502593a2f9bc2ff
Sophiaa1204 <sihanwangsophia@gmail.com>
2024-03-05 20:46:54 -0500

    feat: add comment

---
+++ blog_java_backend/src/main/java/com/sophia/blog_java_backend/controller/CommentController.java
@@ -0,0 +1,31 @@
+package com.sophia.blog_java_backend.controller;
+
+import com.sophia.blog_java_backend.entity.Comment;
+import com.sophia.blog_java_backend.service.CommentService;
+import com.sophia.blog_java_backend.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping(path = "/comment")
+public class CommentController {
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private HostHolder hostHolder;
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        commentService.addComment(comment);
+
+        return "redirect:/discuss/detail/" + discussPostId;
+    }
+}
